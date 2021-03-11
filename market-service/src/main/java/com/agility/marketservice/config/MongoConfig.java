6eeccd49f77159a2f18d41280d71f1fb795e9550
Vuong Thanh Nguyen <vuong.nguyenthanh@asnet.com.vn>
2021-03-11 14:05:00 +0700

    Fix mongo config

--- market-service/src/main/java/com/agility/marketservice/config/MongoConfig.java
+++ market-service/src/main/java/com/agility/marketservice/config/MongoConfig.java
@@ -1,22 +1,54 @@
 package com.agility.marketservice.config;
 
 import com.agility.marketservice.repository.ResourceRepository;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Collection;
 
 @Configuration
 @EnableMongoRepositories(
     basePackages = "com.agility.marketservice.repository",
     repositoryBaseClass = ResourceRepository.class)
 public class MongoConfig extends AbstractMongoClientConfiguration {
+  @Value("${spring.data.mongodb.host}")
+  private String mongoHost;
+
+  @Value("${spring.data.mongodb.port}")
+  private String mongoPort;
+
+  @Value("${spring.data.mongodb.database}")
+  private String mongoDb;
+
   @Override
   protected String getDatabaseName() {
-    return "markets";
+    return mongoDb;
+  }
+
+  @Override
+  protected Collection<String> getMappingBasePackages() {
+    return super.getMappingBasePackages();
   }
 
   @Override
   protected boolean autoIndexCreation() {
     return true;
+  }
+
+  @Override
+  public MongoClient mongoClient() {
+    String uri = "mongodb://" + mongoHost + ":" + mongoPort + "/" + mongoDb;
+    ConnectionString connectionString = new ConnectionString(uri);
+    MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+        .applyConnectionString(connectionString)
+        .build();
+
+    return MongoClients.create(mongoClientSettings);
   }
 }
